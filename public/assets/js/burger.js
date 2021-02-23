a30d5f78c5e9b3ad0267b90e58ee5105ad57233c
Joshua Arroyo <Jalexarroyo@gmail.com>
2021-02-22 17:17:57 -0700

    added css styles and background photo

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -42,19 +42,4 @@ $(function() {
         }
       );
     });
-  
-    // $(".delete-cat").on("click", function(event) {
-    //   var id = $(this).data("id");
-  
-    //   // Send the DELETE request.
-    //   $.ajax("/api/burgers/" + id, {
-    //     type: "DELETE"
-    //   }).then(
-    //     function() {
-    //       console.log("deleted cat", id);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
   });
